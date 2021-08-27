80136cd9ce1eeb191affc747455ca6e1dd2c5d16
Damian Vigo <damiandanielvigo@gmail.com>
2021-08-26 23:35:58 -0300

    validate-2

--- forms/form.php
+++ forms/form.php
@@ -22,7 +22,7 @@ if(isset($_POST['submit'])) {
     if(!empty($correo)) {
        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
        
-        if(var_dump(!filter_var($correo, FILTER_VALIDATE_EMAIL))) {
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
             $errores .= 'Por favor ingresa un correo valido <br />';
         } else {
             echo "Tu correo es: $correo";
---
+++ practices/contact_form/index.view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<body>
+    <div class="wrap">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);  ?>" method="post">
+            <input type="text" class="form-control" name="nombre" placeholder="Nombre:" id="nombre" value="">
+
+            <input type="text" id="correo" class="form-control" name="correo" placeholder="Correo:" value="">
+
+            <textarea id="mensaje" name="mensaje" class="form-control" placeholder="Mensaje:"></textarea>
+
+            <!--    <div class="alert error">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+
+            <div class="alert success">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div> -->
+
+            <?php if (!empty($errores)) : ?>
+                <div class="alert error">
+                    <?php echo $errores; ?>
+                </div>
+            <?php elseif ($enviado) : ?>
+                <div class="alert success">
+                    <p>Enviado correctamente</p>
+                </div>
+            <?php endif ?>
+
+            <input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
+        </form>
+    </div>
+</body>
+
+</html>
