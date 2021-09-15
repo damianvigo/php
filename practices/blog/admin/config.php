c59e6719d97f1729e2da6d45452e09501867ad62
Damian Vigo <damiandanielvigo@gmail.com>
2021-09-14 21:29:15 -0300

    add-header-blog

---
+++ practices/blog/admin/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('RUTA', 'http://localhost/course_php/practices/blog/');
+
+$bd_config = array(
+    'basedatos' => 'blog_practica',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$blog_config = array(
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'img/'
+);
+
+$blog_admin = array(
+    'usuario' => 'dv',
+    'pass' => '123'
+);
